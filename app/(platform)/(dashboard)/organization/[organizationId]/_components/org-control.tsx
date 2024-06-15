1f18d17175ead1f0d69c572d9452ec01001c6506
v_zhujiaxing01@baidu.com <v_zhujiaxing01@baidu.com>
2024-06-16 02:24:02 +0800

    PC Sidebar

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useParams } from "next/navigation"
+import { useOrganizationList } from "@clerk/nextjs"
+import { useEffect } from "react"
+
+export const OrgControl = () => {
+    const params = useParams()
+    const { setActive } = useOrganizationList()
+
+    useEffect(() => {
+        if (!setActive) return
+
+        setActive({
+            organization: params.organizationId as string
+        })
+    }, [setActive, params.organizationId])
+
+    return null
+}
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,16 @@
+import { OrgControl } from "./_components/org-control"
+
+const OrganizationIdLayout = ({
+    children,
+}: {
+    children: React.ReactNode
+}) => {
+    return (
+        <div>
+            <OrgControl />
+            {children}
+        </div>
+    )
+}
+
+export default OrganizationIdLayout
