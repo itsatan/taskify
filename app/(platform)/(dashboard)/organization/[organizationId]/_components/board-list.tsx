f4e99e19eae4c5ac5a0b94e4cedf900ea0399d96
v_zhujiaxing01@baidu.com <v_zhujiaxing01@baidu.com>
2024-06-19 22:29:35 +0800

    Sonner

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import { FormPopover } from "@/components/form/form-popover"
 import { Hint } from "@/components/hint"
 import { HelpCircle, User2 } from "lucide-react"
 
@@ -9,26 +10,28 @@ export const BoardList = () => {
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div
-                    role="button"
-                    className="aspect-video relative w-full h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-                >
-                    <p className="text-sm">
-                        Create new board
-                    </p>
-                    <span className="text-xs">
-                        5 remaining
-                    </span>
-                    <Hint
-                        sideOffset={40}
-                        description={`
+                <FormPopover sideOffset={10} side="right">
+                    <div
+                        role="button"
+                        className="aspect-video relative w-full h-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                    >
+                        <p className="text-sm">
+                            Create new board
+                        </p>
+                        <span className="text-xs">
+                            5 remaining
+                        </span>
+                        <Hint
+                            sideOffset={40}
+                            description={`
                             Free workspaces can have up to 5 open boards. For unlimited
                             boards upgrade this workspace.
                         `}
-                    >
-                        <HelpCircle className="w-[14px] h-[14px] absolute right-2 bottom-2" />
-                    </Hint>
-                </div>
+                        >
+                            <HelpCircle className="w-[14px] h-[14px] absolute right-2 bottom-2" />
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
         </div>
     )
