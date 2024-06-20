4b6f2fbf02e17d82cb0feb8c74e7879b284886a1
v_zhujiaxing01@baidu.com <v_zhujiaxing01@baidu.com>
2024-06-20 19:41:39 +0800

    BoardList

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,28 @@
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
 import { FormPopover } from "@/components/form/form-popover"
 import { Hint } from "@/components/hint"
 import { HelpCircle, User2 } from "lucide-react"
+import { auth } from '@clerk/nextjs/server'
+import Link from 'next/link'
+import { Skeleton } from '@/components/ui/skeleton'
 
-export const BoardList = () => {
+export const BoardList = async () => {
+    const { orgId } = auth()
+
+    if (!orgId) {
+        return redirect('/select-org')
+    }
+
+    const boards = await db.board.findMany({
+        where: {
+            orgId,
+        },
+        orderBy: {
+            createAt: 'desc'
+        }
+    })
+
     return (
         <div className="space-y-4">
             <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +30,17 @@ export const BoardList = () => {
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+                {boards.map((board) => (
+                    <Link
+                        key={board.id}
+                        href={`/boards/${board.id}`}
+                        style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+                        className='group relative w-full h-full aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm p-2 overflow-hidden'
+                    >
+                        <div className='absolute inset-0 bg-black/30 group-hover:bg-black/40 transition' />
+                        <p className='relative font-semibold text-white'>{board.title}</p>
+                    </Link>
+                ))}
                 <FormPopover sideOffset={10} side="right">
                     <div
                         role="button"
@@ -33,6 +64,21 @@ export const BoardList = () => {
                     </div>
                 </FormPopover>
             </div>
+        </div>
+    )
+}
+
+BoardList.Skeleton = function SkeletonBoardList() {
+    return (
+        <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4'>
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
+            <Skeleton className='aspect-video w-full h-full p-2' />
         </div>
     )
 }
