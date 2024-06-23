f8c549f1c4205e8f278d863d6e77b4f217da666f
v_zhujiaxing01@baidu.com <v_zhujiaxing01@baidu.com>
2024-06-23 23:28:02 +0800

    CardModalHeader

---
+++ actions/update-card/schema.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod'
+
+export const UpdateCard = z.object({
+    id: z.string(),
+    boardId: z.string(),
+    description: z.optional(
+        z.string({
+            required_error: 'Description is Required',
+            invalid_type_error: 'Description must be a string'
+        }).min(3, {
+            message: 'Description is too short'
+        })
+    ),
+    title: z.string({
+        required_error: 'Title is Required',
+        invalid_type_error: 'Title must be a string',
+    }).min(3, {
+        message: 'Title is too short'
+    }),
+})
--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -2,6 +2,7 @@
 
 import { Card } from "@prisma/client";
 import { Draggable } from '@hello-pangea/dnd'
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
     index: number;
@@ -12,6 +13,9 @@ export const CardItem = ({
     index,
     data
 }: CardItemProps) => {
+
+    const cardModal = useCardModal()
+
     return (
         <Draggable draggableId={data.id} index={index}>
             {(provided) => (
@@ -21,6 +25,7 @@ export const CardItem = ({
                     ref={provided.innerRef}
                     role="button"
                     className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
+                    onClick={() => cardModal.onOpen(data.id)}
                 >
                     {data.title}
                 </div>
